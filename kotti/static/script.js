40873c89e4972832bdcb931209f7cb9eaff6e8b9
Daniel Nouri <daniel.nouri@gmail.com>
2011-01-28 18:16:39 +0100

    Add form unload protection

--- kotti/static/script.js
+++ kotti/static/script.js
@@ -1,8 +1,21 @@
 (function($) {
 
-     $(document).ready(function() {
-         deform.load();
-     });
+    function dirty_forms() {
+        function forms() { return $("form").not("[class~=dirty-ignore]"); }
+        var initial = forms().serialize();
+        $(window).bind('beforeunload', function() {
+            if (tinyMCE)
+                tinyMCE.triggerSave(true);
+            if ($("form").serialize() != initial) {
+                return "Your changes have not been saved.\nAre you sure you want to leave this page?"
+            }
+        });
+    }
+
+    $(document).ready(function() {
+        deform.load();
+        dirty_forms();
+    });
 
 
  })(jQuery);
