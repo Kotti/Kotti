63265df178f5497e1a6173ad47a21032b1b95a75
Daniel Nouri <daniel.nouri@gmail.com>
2011-02-03 13:17:58 +0100

    Improve context menu visibility.

--- kotti/static/script.js
+++ kotti/static/script.js
@@ -2,15 +2,16 @@
 
     function dirty_forms() {
         function forms() { return $("form").not("[class~=dirty-ignore]"); }
-        
+
         forms().submit(function() { $(window).unbind('beforeunload'); });
         var initial = forms().serialize();
         $(window).bind("beforeunload", function() {
             if (tinyMCE)
                 tinyMCE.triggerSave(true);
             if ($("form").serialize() != initial) {
-                return "Your changes have not been saved.\nAre you sure you want to leave this page?"
+                return "Your changes have not been saved.\nAre you sure you want to leave this page?";
             }
+        return null;
         });
     }
 
