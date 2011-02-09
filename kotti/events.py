bb42dd80a0d0915e3f29e575f696f26604ba3da5
Daniel Nouri <daniel.nouri@gmail.com>
2011-02-09 19:21:05 +0100

    Add a sqlalchemy.event to set owner of Node if unset.

--- kotti/events.py
+++ kotti/events.py
@@ -2,6 +2,19 @@ from datetime import datetime
 
 import sqlalchemy.event
 from sqlalchemy.orm import mapper
+from pyramid.threadlocal import get_current_request
+from pyramid.security import authenticated_userid
+
+from kotti.resources import Node
+
+def set_owner(mapper, connection, target):
+    request = get_current_request()
+    if (request is not None and
+        isinstance(target, Node) and
+        target.owner is None):
+        userid = authenticated_userid(request)
+        if userid is not None: # XXX testme
+            target.owner = userid
 
 def set_creation_date(mapper, connect, target):
     if target.creation_date is None:
@@ -13,5 +26,6 @@ def set_modification_date(mapper, connect, target):
     target.modification_date = datetime.now()
 
 def includeme(config):
+    sqlalchemy.event.listen(mapper, 'before_insert', set_owner)
     sqlalchemy.event.listen(mapper, 'before_insert', set_creation_date)
     sqlalchemy.event.listen(mapper, 'before_update', set_modification_date)
