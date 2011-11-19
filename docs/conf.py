a3aee0eed42679bd54fdca5a00441b9b5eb7e179
Mike Orr <sluggoster@gmail.com>
2011-11-18 21:29:38 -0800

    Add author.

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Kotti CMS'
-copyright = u'2011, Daniel Nouri'
+copyright = u'2011, Daniel Nouri and Mike Orr'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
