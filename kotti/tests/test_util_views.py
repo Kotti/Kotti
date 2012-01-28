906fcbd774c3e9c02fc8777f018f43e642bdb546
Daniel Nouri <daniel.nouri@gmail.com>
2012-01-29 00:58:29 +0100

    Make tests compatible with mock 0.7

--- kotti/tests/test_util_views.py
+++ kotti/tests/test_util_views.py
@@ -140,6 +140,7 @@ class TestTemplateAPI(UnitTestBase):
 
     @patch('kotti.views.util.get_renderer')
     def test_macro(self, get_renderer):
+        get_renderer().implementation().macros = MagicMock()
         api = self.make()
         macro = api.macro('mypackage:mytemplate.pt')
         get_renderer.assert_called_with('mypackage:mytemplate.pt')
@@ -148,6 +149,7 @@ class TestTemplateAPI(UnitTestBase):
     @patch('kotti.views.util.get_renderer')
     def test_macro_bare_with_master(self, get_renderer):
         # getting EDIT_MASTER when bare=True will return BARE_MASTER
+        get_renderer().implementation().macros = MagicMock()
         api = self.make(bare=True)
         macro = api.macro(api.EDIT_MASTER)
         get_renderer.assert_called_with(api.BARE_MASTER)
@@ -156,6 +158,7 @@ class TestTemplateAPI(UnitTestBase):
     @patch('kotti.views.util.get_renderer')
     def test_macro_bare_without_master(self, get_renderer):
         # getting other templates when bare=True
+        get_renderer().implementation().macros = MagicMock()
         api = self.make(bare=True)
         macro = api.macro('mypackage:mytemplate.pt')
         get_renderer.assert_called_with('mypackage:mytemplate.pt')
