78beddb8292a516b2c85b56ceb67f50e1c8b6f5e
Daniel Nouri <daniel.nouri@gmail.com>
2012-02-03 18:27:35 +0100

    Version increment.

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ class PyTest(Command):
         raise SystemExit(errno)
 
 setup(name='Kotti',
-      version='0.5.0a4',
+      version='0.5.0a5',
       description="A user-friendly, light-weight and extensible web content management system.  Written in Python, based on Pyramid and SQLAlchemy.",
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
