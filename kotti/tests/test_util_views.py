bfe4caa81492c3ae045911fefbfcfb93df2db2e2
Nuno Teixeira <nteixeira@consipere.com>
2012-03-02 18:54:19 +0000

    Move test about using non-ASCII characters on settings values to `kotti.tests.test_app`

--- kotti/tests/test_util_views.py
+++ kotti/tests/test_util_views.py
@@ -58,12 +58,6 @@ class TestTemplateAPI(UnitTestBase):
         api = self.make()
         self.assertEqual(api.site_title, u'This is it.')
 
-    @patch('kotti.views.util.get_settings')
-    def test_site_title_with_non_ascii_characters(self, get_settings):
-        get_settings.return_value = {'kotti.site_title': u'Kötti'}
-        api = self.make()
-        self.assertEqual(api.site_title, u'Kötti')
-
     @patch('kotti.views.util.has_permission')
     def test_list_children(self, has_permission):
         has_permission.return_value = True
