17f986f5f4ca075f1445dbf37b8052c056ff1d95
Daniel Nouri <daniel.nouri@gmail.com>
2012-04-21 20:19:13 +0200

    Version increment

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ if sys.version_info[:3] < (2,7,0):
     install_requires.append('ordereddict')
 
 setup(name='Kotti',
-      version='0.6.2',
+      version='0.6.3dev',
       description="Kotti is a high-level, 'Pythonic' web application framework. It includes a small and extensible CMS application called the Kotti CMS.",
       long_description='\n\n'.join([README, THANKS, CHANGES]),
       classifiers=[
