617a8748d362de9dafdfa0cab73e571c07e97bd9
Marco Scheidhuber <marco@jusid.de>
2012-05-30 22:13:41 +0200

    Add test to check that the File content type is also searched, because it derives from Content.

--- kotti/tests/test_search.py
+++ kotti/tests/test_search.py
@@ -4,13 +4,15 @@ from kotti.testing import UnitTestBase
 
 def create_contents(root=None):
     from kotti.resources import get_root
-    from kotti.resources import Content
+    from kotti.resources import Content, File
     if root is None:
         root = get_root()
     doc1 = root['doc1'] = Content(title=u'First Document')
     doc11 = root['doc1']['doc11'] = Content(title=u'Second Document')
     doc12 = root['doc1']['doc12'] = Content(title=u'Third Document')
-    return doc1, doc11, doc12
+    file1 = root['doc1']['file1'] = File(title=u'First File',
+                                         description=u'this is a file')
+    return doc1, doc11, doc12, file1
 
 
 class TestSearch(UnitTestBase):
@@ -24,7 +26,7 @@ class TestSearch(UnitTestBase):
     def test_search_content(self):
         from kotti.views.util import search_content
         request = DummyRequest()
-        doc1, doc11, doc12 = create_contents()
+        doc1, doc11, doc12, file1 = create_contents()
         results = search_content(u'First Document', request)
         assert len(results) == 1
         assert results[0]['name'] == u'doc1'
@@ -35,10 +37,20 @@ class TestSearch(UnitTestBase):
         assert results[1]['title'] == 'Second Document'
         assert results[1]['path'] == '/doc1/doc11/'
 
+    def test_search_file_description(self):
+        from kotti.views.util import search_content
+        request = DummyRequest()
+        doc1, doc11, doc12, file1 = create_contents()
+        results = search_content(u'this is a file', request)
+        assert len(results) == 1
+        assert results[0]['name'] == 'file1'
+        assert results[0]['title'] == 'First File'
+        assert results[0]['path'] == '/doc1/file1/'
+
     def test_search_content_without_permission(self):
         from kotti.views.util import search_content
         request = DummyRequest()
-        doc1, doc11, doc12 = create_contents()
+        create_contents()
         self.config.testing_securitypolicy(permissive=False)
         results = search_content(u'Document', request)
         assert len(results) == 0
