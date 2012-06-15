8e55cc07598b58a47db0cac7c2f4fa6dd691be2e
Andreas kaiser <disko@binary-punks.com>
2012-06-15 11:15:54 +0200

    use pkg_resources.resource_filename instead of os.path

--- kotti/static.py
+++ kotti/static.py
@@ -1,33 +1,49 @@
-import deform
-import deform_bootstrap
-import os
 from fanstatic import Group, Library, Resource
 from js.bootstrap import bootstrap_js
 from js.bootstrap import bootstrap_responsive_css
 from js.jquery import jquery
 from js.jquery_timepicker_addon import timepicker
 from js.jqueryui_tagit import tagit
+from pkg_resources import resource_filename
+
 
 # deform doesn't provide fanstatic resources itself
-lib_deform = Library("deform", os.sep.join((os.path.dirname(deform.__file__), "static")))
+deform_dir = resource_filename("deform", "static")
+lib_deform = Library("deform", deform_dir)
 deform_js = Resource(lib_deform, "scripts/deform.js")
-jquery_form_js = Resource(lib_deform, "scripts/jquery.form.js", depends=[jquery, ])
+jquery_form_js = Resource(
+    lib_deform,
+    "scripts/jquery.form.js",
+    depends=[jquery, ])
 
 # deform_bootstrap doesn't provide fanstatic resources itself
 # this should be moved to deform_bootstrap in the future
-lib_deform_bootstrap = Library("deform_bootstrap", os.sep.join((os.path.dirname(deform_bootstrap.__file__), "static")))
-deform_bootstrap_js = Resource(lib_deform_bootstrap, "deform_bootstrap.js", depends=[deform_js, ])
+deform_bootstrap_dir = resource_filename("deform_bootstrap", "static")
+lib_deform_bootstrap = Library("deform_bootstrap", deform_bootstrap_dir)
+deform_bootstrap_js = Resource(
+    lib_deform_bootstrap,
+    "deform_bootstrap.js",
+    depends=[deform_js, ])
 
 # Kotti's resources
 lib_kotti = Library("kotti", "static")
-kotti_js = Resource(lib_kotti, "kotti.js", bottom=True)
-base_css = Resource(lib_kotti, "base.css", depends=[bootstrap_responsive_css, ])
-edit_css = Resource(lib_kotti, "edit.css", depends=[bootstrap_responsive_css, ])
-view_css = Resource(lib_kotti, "view.css", depends=[base_css, ])
+kotti_js = Resource(lib_kotti,
+    "kotti.js",
+    bottom=True)
+base_css = Resource(lib_kotti,
+    "base.css",
+    depends=[bootstrap_responsive_css, ])
+edit_css = Resource(lib_kotti,
+    "edit.css",
+    depends=[bootstrap_responsive_css, ])
+view_css = Resource(lib_kotti,
+    "view.css",
+    depends=[base_css, ])
 
 
 class NeededGroup(object):
-    """A collection of fanstatic resources that supports dynamic appending of resources after initialization"""
+    """A collection of fanstatic resources that supports
+       dynamic appending of resources after initialization"""
 
     def __init__(self, resources=[]):
 
@@ -59,5 +75,16 @@ class NeededGroup(object):
         # we should add browser tests for view_needed and edit_needed (see below)
         Group(self.resources).need()
 
-view_needed = NeededGroup([jquery, bootstrap_js, view_css])
-edit_needed = NeededGroup([jquery, bootstrap_js, edit_css, tagit, kotti_js, timepicker, jquery_form_js, deform_bootstrap_js, ])
+view_needed = NeededGroup([
+    jquery,
+    bootstrap_js,
+    view_css])
+edit_needed = NeededGroup([
+    jquery,
+    bootstrap_js,
+    edit_css,
+    tagit,
+    kotti_js,
+    timepicker,
+    jquery_form_js,
+    deform_bootstrap_js, ])
