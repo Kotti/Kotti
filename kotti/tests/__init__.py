3630e3d0d0aecd5b4aaec00098593a98884b8137
Andreas Zeidler <az@zitc.de>
2012-07-15 17:04:48 +0200

    remove ``kotti/tests/__init__.py``
    it is not required when using 'py.test' and also breaks test coverage reports

--- kotti/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-from warnings import warn
-
-
-warn('Importing from `kotti.tests` is no longer possible. '
-     'Please change your code to import from `kotti.testing` instead.',
-     ImportWarning)
