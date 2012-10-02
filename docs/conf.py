6f6310e7057692ce1d1b0fa8aaf2ec75c73af650
Daniel Nouri <daniel.nouri@gmail.com>
2012-10-02 18:42:02 +0200

    Update docs version

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ copyright = u'2012, Daniel Nouri and contributors'
 # built documents.
 #
 # The short X.Y version.
-version = '0.6'
+version = '0.7'
 # The full version, including alpha/beta/rc tags.
 release = version
 
