06a6ad47f1ad67c4a262a290db6ffa006e43877e
Andreas kaiser <disko@binary-punks.com>
2012-10-04 21:23:31 +0200

    Make selectable_default_views a list of 2-tuples containing (view_name, view_title) pairs.

--- kotti/views/edit/default_view_selection.py
+++ kotti/views/edit/default_view_selection.py
@@ -19,7 +19,7 @@ class DefaultViewSelection(object):
         self.context = context
         self.request = request
 
-    def _is_valid_view(self, view_name):
+    def _get_view(self, view_name):
         """This code is copied from pyramid.view.
            Returns True if a view with name view_name is registered for context.
         """
@@ -33,20 +33,26 @@ class DefaultViewSelection(object):
             reg = self.request.registry
         except AttributeError:
             reg = get_current_registry()
-        view = reg.adapters.lookup(provides, IView, name=view_name)
 
-        return view is not None
+        return reg.adapters.lookup(provides, IView, name=view_name)
 
-    @view_config(name='default-view-selector', renderer='kotti:templates/default-view-selector.pt')
+    def _is_valid_view(self, view_name):
+        """Return True if a view with name view_name is registered for context.
+        """
+
+        return self._get_view(view_name) is not None
+
+    @view_config(name='default-view-selector',
+                 renderer='kotti:templates/default-view-selector.pt')
     def default_view_selector(self):
         """Submenu for selection of the node's default view.
         """
 
         sviews = [
             {
-                "name": v,
-                "title": v,
-                "is_current": v == self.context.default_view,
+                "name": v[0],
+                "title": v[1],
+                "is_current": v[0] == self.context.default_view,
             }
             for v in self.context.type_info.selectable_default_views
         ]
