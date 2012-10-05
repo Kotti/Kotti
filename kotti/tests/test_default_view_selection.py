63a7203d46eea6af89e7032c3eb5dd5330bd9b8d
Andreas kaiser <disko@binary-punks.com>
2012-10-05 22:15:45 +0200

    Make tests work.

--- kotti/tests/test_default_view_selection.py
+++ kotti/tests/test_default_view_selection.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import warnings
 from kotti.resources import get_root
+from kotti.resources import IContent
 from kotti.testing import DummyRequest
 from kotti.testing import UnitTestBase
 from kotti.views.edit.default_view_selection import DefaultViewSelection
@@ -11,15 +13,29 @@ class TestDefaultViewSelection(UnitTestBase):
 
     def test__is_valid_view(self):
 
+        self.config.add_view(
+            context=IContent,
+            name='folder_view',
+            permission='view',
+            renderer='kotti:templates/view/folder.pt',
+        )
+
         context = get_root()
         request = DummyRequest()
 
         view = DefaultViewSelection(context, request)
 
-        #assert view._is_valid_view("folder_view") == True
-        #assert view._is_valid_view("foo_view") == False
+        assert view._is_valid_view("folder_view") == True
+        assert view._is_valid_view("foo_view") == False
 
     def test_default_view_selection(self):
+
+        self.config.add_view(
+            context=IContent,
+            name='folder_view',
+            permission='view',
+            renderer='kotti:templates/view/folder.pt',
+        )
 
         context = get_root()
         request = DummyRequest()
@@ -31,7 +47,7 @@ class TestDefaultViewSelection(UnitTestBase):
         assert 'selectable_default_views' in sviews
 
         # the root should have at least the default view and te folder_view
-        assert len(sviews['selectable_default_views']) == 2
+        assert len(sviews['selectable_default_views']) > 1
 
         # the first view is always the default view
         assert sviews['selectable_default_views'][0]['is_current'] is True
@@ -45,4 +61,28 @@ class TestDefaultViewSelection(UnitTestBase):
         view = DefaultViewSelection(context, request)
 
         assert type(view.set_default_view()) == HTTPFound
-        #assert context.default_view == 'folder_view'
+        assert context.default_view == 'folder_view'
+
+        # set back to default
+        request = DummyRequest(GET={'view_name': 'default'})
+        view = DefaultViewSelection(context, request)
+
+        assert type(view.set_default_view()) == HTTPFound
+        assert context.default_view is None
+
+        # try to set non existing view
+        request = DummyRequest(GET={'view_name': 'nonexisting'})
+        view = DefaultViewSelection(context, request)
+
+        assert type(view.set_default_view()) == HTTPFound
+        assert context.default_view is None
+
+    def test_warning_for_non_registered_views(self):
+
+        with warnings.catch_warnings(record=True) as w:
+
+            DefaultViewSelection(get_root(), DummyRequest()).default_view_selector()
+
+            assert len(w) == 1
+            assert issubclass(w[-1].category, UserWarning)
+            assert str(w[-1].message) == "No view called 'folder_view' is registered for <Document 1 at />"
