da56323ea553537fbd1010385bf834a4f13ec8b2
Marco Scheidhuber <marco@jusid.de>
2012-10-24 10:34:52 +0200

    Remove unused import.

--- kotti/views/view.py
+++ kotti/views/view.py
@@ -7,7 +7,6 @@ from kotti.resources import IContent
 from kotti.resources import Document
 
 from kotti.views.util import search_content
-from kotti.views.edit import contents
 
 
 def view_content_default(context, request):
