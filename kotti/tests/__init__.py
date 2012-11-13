fbf7f78d2e4873557c3c5a1351d9efc1ada40f79
Andreas kaiser <disko@binary-punks.com>
2012-11-13 10:03:22 +0100

    Make Kotti's test fixtures/funcargs a plugin usable by third party add-ons.

---
+++ kotti/tests/__init__.py
@@ -0,0 +1,4 @@
+# Needed to make fixtures and funcargs in conftest.py available to third party
+# packages' tests.
+#
+# see http://pytest.org/latest/plugins.html#making-your-plugin-installable-by-others
