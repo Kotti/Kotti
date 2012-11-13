0b509602c96d82c4f9b558d0a0ea41e1e2b9f087
Daniel Nouri <daniel.nouri@gmail.com>
2012-11-13 13:58:10 +0100

    Release 0.8a1

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ if sys.version_info[:3] < (2, 7, 0):
     install_requires.append('ordereddict')
 
 setup(name='Kotti',
-      version='0.8.0dev',
+      version='0.8a1',
       description="Kotti is a high-level, 'Pythonic' web application framework. It includes a small and extensible CMS application called the Kotti CMS.",
       long_description='\n\n'.join([README, AUTHORS, CHANGES]),
       classifiers=[
