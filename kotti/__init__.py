cafb820489228287d1981ade78c01eb971cdc312
Nuno Teixeira <nteixeira@consipere.com>
2013-09-13 10:38:13 +0100

    Add a new setting ``kotti.allow_admin_set_passwords``

--- kotti/__init__.py
+++ kotti/__init__.py
@@ -118,6 +118,7 @@ conf_defaults = {
     'kotti.register': 'False',
     'kotti.register.group': '',
     'kotti.register.role': '',
+    'kotti.allow_admin_set_passwords': 'False',
     'pyramid_deform.template_search_path': 'kotti:templates/deform',
     }
 
