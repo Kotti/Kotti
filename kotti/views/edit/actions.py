b9dc01a16fa83b94112e69477b37d244e139211d
Marco Scheidhuber <j23d@jusid.de>
2014-05-16 21:54:59 +0200

    Revert commit:4a38d8f0 as it leads to permission problems, fixes #309.

--- kotti/views/edit/actions.py
+++ kotti/views/edit/actions.py
@@ -550,7 +550,7 @@ def move_child_position(context, request):
     return {'result': result}
 
 
-@view_config(name='workflow-dropdown', permission='state_change',
+@view_config(name='workflow-dropdown', permission='view',
              renderer='kotti:templates/workflow-dropdown.pt')
 def workflow(context, request):
     """
