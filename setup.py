801101db47bf42252671623a4cb6b12e81819de2
Daniel Nouri <daniel.nouri@gmail.com>
2014-06-11 11:34:17 +0200

    Release 0.10a3

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ if sys.version_info[:3] < (2, 7, 0):
     install_requires.append('ordereddict')
 
 setup(name='Kotti',
-      version='0.10a3dev',
+      version='0.10a3',
       description="A user-friendly, light-weight and extensible web content management system. Based on Pyramid and SQLAlchemy.",
       long_description='\n\n'.join([README, AUTHORS, CHANGES]),
       classifiers=[
