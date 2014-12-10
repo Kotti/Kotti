911277193cc36a9d6a6f793379320e57fc3c8d57
Andreas Kaiser <disko@binary-punks.com>
2014-12-10 13:27:35 +0100

    Resolve #320 and add related migration step.

---
+++ kotti/alembic/versions/559ce6eb0949_update_node_path_column.py
@@ -0,0 +1,30 @@
+"""Update Node.path column
+
+Revision ID: 559ce6eb0949
+Revises: 1063d7178fa
+Create Date: 2014-12-10 13:20:29.374951
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '559ce6eb0949'
+down_revision = '1063d7178fa'
+
+
+def upgrade():
+
+    from kotti import DBSession
+    from kotti.resources import Node
+
+    for node in DBSession.query(Node).with_polymorphic([Node]):
+        # append '/' to all nodes but root
+        node.path != u'/' and node.path += u'/'
+
+
+def downgrade():
+    from kotti import DBSession
+    from kotti.resources import Node
+
+    for node in DBSession.query(Node).with_polymorphic([Node]):
+        # remove trailing '/' from all nodes but root
+        node.path == u'/' or node.path[:-1]
