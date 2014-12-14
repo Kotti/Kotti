b0da0a6ded9b3879a230cb9f5ef7f33873d662bd
Tiberiu Ichim <tibi@pixelblaster.ro>
2014-12-14 12:20:36 -0800

    Added test for UploadedFileResponse; Parametrized the TestFileViews

--- kotti/views/file.py
+++ kotti/views/file.py
@@ -36,6 +36,7 @@ class UploadedFileResponse(Response):
                  content_encoding=None):
 
         filename = data.filename
+        content_type = content_type or getattr(data, 'content_type', None)
 
         if content_type is None:
             content_type, content_encoding = mimetypes.guess_type(
