Kotti/Kotti

commit c7ade7bab05e53d73f8089871afe1337071c0458

Use camel case in variable names

kotti/static/upload.js
@@ -31,7 +31,7 @@ function UploadController($scope, $http, $log) {
 
     $scope.files = [];
     $scope.errors = [];
-    $scope.num_files_waiting = 0;
+    $scope.numFilesWaiting = 0;
 
     $scope.uploadAll = function () {
         $scope.uploader.uploadStoredFiles();
@@ -69,10 +69,10 @@ function UploadController($scope, $http, $log) {
                 $scope.apply(function () {
                     var file = $scope.uploader.getFile(id);
                     $http.get(
-                        $scope.endpoints.content_types,
+                        $scope.endpoints.contentTypes,
                         {params: {mimetype: file.type}}
                     ).success(function (data, status, headers, config) {
-                        var content_types = data.content_types,
+                        var contentTypes = data.contentTypes,
                             file = {
                                 id: id,
                                 name: name,
@@ -80,7 +80,7 @@ function UploadController($scope, $http, $log) {
                                 file: $scope.uploader.getFile(id)
                             };
 
-                        if (content_types.length === 0) {
+                        if (contentTypes.length === 0) {
                             // todo: display meaningful error message
                             file.status = 'Error';
                             file.error = 'There is no content type in this context that knows create items from that file type.';
@@ -90,11 +90,11 @@ function UploadController($scope, $http, $log) {
 
                         file.status = 'ready for upload';
                         file.transfered = {bytes: 0, percent: 0};
-                        file.allowed_types = content_types;
-                        file.desired_type = content_types[0];
+                        file.allowedTypes = contentTypes;
+                        file.desiredType = contentTypes[0];
 
                         $scope.files.splice(id, 0, file);
-                        $scope.num_files_waiting += 1;
+                        $scope.numFilesWaiting += 1;
                     });
                 });
             },
@@ -103,9 +103,9 @@ function UploadController($scope, $http, $log) {
                 $scope.apply(function () {
                     $scope.files[id].status = 'uploading';
                     $scope.uploader.setParams({
-                        content_type: $scope.files[id].desired_type.name
+                        contentType: $scope.files[id].desiredType.name
                     }, id);
-                    $scope.num_files_waiting -= 1;
+                    $scope.numFilesWaiting -= 1;
                 });
             },
             onProgress: function (id, name, uploadedBytes, totalBytes) {

kotti/static/kotti.js
@@ -4,7 +4,7 @@
 
 "use strict";
 var kotti = {
-    dom_changed_handlers: []
+    domChangedHandlers: []
 };
 var jq = jQuery;
 
@@ -15,7 +15,7 @@ var jq = jQuery;
         return this.filter(selector).add(this.find(selector));
     };
 
-    kotti.dirty_forms = function (node) {
+    kotti.dirtyForms = function (node) {
         var forms = $("form").not("[class~=dirty-ignore]"),
             initial = forms.serialize();
 
@@ -35,8 +35,8 @@ var jq = jQuery;
         });
     };
 
-    kotti.dom_changed = function (node) {
-        $.each(kotti.dom_changed_handlers, function (index, func) {
+    kotti.domChanged = function (node) {
+        $.each(kotti.domChangedHandlers, function (index, func) {
             func(node);
         });
     };
@@ -48,14 +48,14 @@ var jq = jQuery;
             ];
         }
         $.each(handlers, function (index, func) {
-            kotti.dom_changed_handlers.push(func);
+            kotti.domChangedHandlers.push(func);
         });
-        kotti.dom_changed(node);
+        kotti.domChanged(node);
     };
 
     // deform might be undefined, e.g. in kotti_tinymce's kottibrowser
     if (window.deform) {
-      deform.load();
+        deform.load();
     }
 
     kotti.main();

commit 9f5d0d61a1bdb26f04638f9cab63b9a5f98bd655

Fix regression introduced in c7ade7bab05e53d73f8089871afe1337071c0458.  This fixes #425.

kotti/static/upload.js
@@ -69,10 +69,10 @@ function UploadController($scope, $http, $log) {
                 $scope.apply(function () {
                     var file = $scope.uploader.getFile(id);
                     $http.get(
-                        $scope.endpoints.contentTypes,
+                        $scope.endpoints.content_types,
                         {params: {mimetype: file.type}}
                     ).success(function (data, status, headers, config) {
-                        var contentTypes = data.contentTypes,
+                        var contentTypes = data.content_types,
                             file = {
                                 id: id,
                                 name: name,
@@ -103,7 +103,7 @@ function UploadController($scope, $http, $log) {
                 $scope.apply(function () {
                     $scope.files[id].status = 'uploading';
                     $scope.uploader.setParams({
-                        contentType: $scope.files[id].desiredType.name
+                        content_type: $scope.files[id].desiredType.name
                     }, id);
                     $scope.numFilesWaiting -= 1;
                 });