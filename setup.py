d010e15182a90aaca63f4a30a3f84efc526bfa7c
Andreas Kaiser <disko@binary-punks.com>
2015-05-18 17:23:59 +0200

    Use latest ``kotti_tinymce`` in ``requirements.txt``, don't require it at all in setup.py.

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ install_requires = [
     'js.jquery_timepicker_addon',
     'js.jqueryui>=1.8.24',
     'js.jqueryui_tagit',
-    'kotti_tinymce>=0.5dev',
     'lingua>=1.3',
     'plone.scale',  # needed for image resizing capabilities
     'py_bcrypt',
