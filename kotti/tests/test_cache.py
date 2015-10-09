c9e6bc1769d339f233384642b351b9acf01c645e
Andreas Kaiser <disko@binary-punks.com>
2015-10-10 01:31:03 +0200

    Replace browser-cache.txt with equivalent webtest tests.

--- kotti/tests/test_cache.py
+++ kotti/tests/test_cache.py
@@ -1,11 +1,32 @@
 import datetime
+import time
 
 from mock import patch
 from mock import MagicMock
 import pytest
 
+from kotti.resources import File
+from kotti.resources import Image
+from kotti.testing import asset
 from kotti.testing import Dummy
 from kotti.views.cache import set_max_age
+
+
+def parse_expires(date_string):
+    return datetime.datetime(*(
+        time.strptime(date_string, "%a, %d %b %Y %H:%M:%S GMT")[0:6]))
+
+
+def delta(date_string):
+    now = datetime.datetime.utcnow()
+    return parse_expires(date_string) - now
+
+
+@pytest.fixture
+def cachetest_content(root, filedepot):
+    image = asset('sendeschluss.jpg').read()
+    root['textfile'] = File("file contents", u"mytext.txt", u"text/plain")
+    root['image'] = Image(image, u"sendeschluss.jpg", u"image/jpeg")
 
 
 class TestSetMaxAge:
@@ -76,3 +97,61 @@ class TestSetCacheHeaders:
             set_cache_headers(event)
 
             assert chooser.call_count == 0
+
+
+class TestBrowser:
+
+    def test_cache_unauth(self, webtest, cachetest_content):
+
+        # html
+        resp = webtest.app.get('/')
+        assert resp.headers.get('X-Caching-Policy') == 'Cache HTML'
+        assert resp.headers.get('Cache-Control') == 'max-age=0,s-maxage=3600'
+        d = delta(resp.headers.get('Expires'))
+        assert (d.days, d.seconds) < (0, 0)
+
+        # media content
+        resp = webtest.app.get('/textfile/inline-view')
+        assert resp.headers.get('X-Caching-Policy') == 'Cache Media Content'
+        assert resp.headers.get('Cache-Control') == 'max-age=14400'
+        d = delta(resp.headers.get('Expires'))
+        assert (d.days, d.seconds) > (0, 14000)
+        resp = webtest.app.get('/image/inline-view')
+        assert resp.headers.get('X-Caching-Policy') == 'Cache Media Content'
+
+        # resources
+        resp = webtest.app.get('/static-kotti/base.css')
+        assert resp.headers.get('X-Caching-Policy') == 'Cache Resource'
+        assert resp.headers.get('Cache-Control') == 'max-age=2764800,public'
+        d = delta(resp.headers.get('Expires'))
+        assert (d.days, d.seconds) > (30, 0)
+        assert 'Last-Modified' in resp.headers
+
+        # post
+        resp = webtest.app.post('/', '')
+        assert 'X-Caching-Policy' not in resp.headers
+
+        # 404
+        resp = webtest.app.get('/this-isnt-here', status=404)
+        assert 'X-Caching-Policy' not in resp.headers
+
+    @pytest.mark.user('admin')
+    def test_cache_auth(self, webtest, cachetest_content):
+
+        # html
+        resp = webtest.app.get('/')
+        assert resp.headers.get('X-Caching-Policy') == 'No Cache'
+
+        # media content
+        resp = webtest.app.get('/textfile/inline-view')
+        assert resp.headers.get('X-Caching-Policy') == 'No Cache'
+        resp = webtest.app.get('/image/inline-view')
+        assert resp.headers.get('X-Caching-Policy') == 'No Cache'
+
+        # resources
+        resp = webtest.app.get('/static-kotti/base.css')
+        assert resp.headers.get('X-Caching-Policy') == 'Cache Resource'
+
+        # 404
+        resp = webtest.app.get('/this-isnt-here', status=404)
+        assert 'X-Caching-Policy' not in resp.headers
