62db9e3650aff625112bfec7218a9ecfa4892e2e
Tiberiu Ichim <tibi@pixelblaster.ro>
2016-10-28 18:54:39 +0300

    Fix a bug when TypeInfo.add_view on a content type is None

--- kotti/tests/test_util_views.py
+++ kotti/tests/test_util_views.py
@@ -146,6 +146,43 @@ class TestTemplateAPI:
             api.has_permission('drink')
             has_permission.assert_called_with('drink', api.root)
 
+    def test_add_links(self, config):
+        from kotti.views.edit.actions import content_type_factories
+        from kotti.resources import Document, File
+        from kotti.views.edit import content
+
+        config.include(content)
+
+        res = content_type_factories(Document(''), DummyRequest())
+        assert res['factories'] == [Document, File]
+
+    def test_add_links_invalid_factory(self, config):
+        from kotti.resources import Content, Document, File
+        from kotti.resources import default_type_info
+        from kotti.views.edit import content
+        from kotti.views.edit.actions import content_type_factories
+        from sqlalchemy import Column, Integer, ForeignKey
+
+        class TestContent(Content):
+            id = Column(Integer, ForeignKey('contents.id'), primary_key=True)
+
+            type_info = default_type_info.copy(
+                name=u'TestContent',
+                title=u'Test Content',
+                add_view=None,
+                addable_to=[u'Document'],
+            )
+
+        config.include(content)
+        config.registry.settings['kotti.available_types'].append(TestContent)
+        res = content_type_factories(Document(''), DummyRequest())
+
+        assert res['factories'] == [Document, File]
+
+        TestContent.type_info.add_view = 'add_document'
+        res = content_type_factories(Document(''), DummyRequest())
+        assert res['factories'] == [Document, File, TestContent]
+
     def test_edit_links(self, config, db_session):
         from kotti import views
         from kotti.views.edit import actions, content, default_views
