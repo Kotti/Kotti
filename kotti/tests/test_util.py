7b3d19a6194e6114c1fb36923fa26dbc5994d257
Tiberiu Ichim <tibi@pixelblaster.ro>
2016-11-06 22:20:08 +0200

    Simplify Link.selected code, use request.view_name instead of request.url

--- kotti/tests/test_util.py
+++ kotti/tests/test_util.py
@@ -140,44 +140,15 @@ class TestLink:
         from kotti.testing import DummyRequest
 
         req = DummyRequest()
-        req.url = "http://example.com/@@manage"
+        req.view_name = "manage"
 
         assert Link('manage').selected(Mock(__name__=None), req)
 
-        req.url = "http://example.com/@@manage_cats"
+        req.view_name = 'manage_cats'
         assert not Link('manage').selected(Mock(__name__=None), req)
 
-    def test_link_selected_no_view_markers(self):
-        from kotti.util import Link
-        from kotti.testing import DummyRequest
-        from mock import Mock
-
-        req = DummyRequest()
-        root = Mock(__name__=None)
-        manage = Mock(__name__='manage',
-                      __parent__=Mock(__name__=None))
-
-        req.url = "http://example.com/manage"
-        assert Link('manage').selected(root, req)
-
-        req.url = "http://example.com/manage/"
-        assert not Link('manage').selected(root, req)
-
-        req.url = "http://example.com/"
-        assert Link('').selected(root, req)
-
-        req.url = "http://example.com/manage/"
-        link = Link('')
-        assert link.selected(manage, req)
-
-        req.url = "http://example.com/manage"
-        assert not link.selected(manage, req)
-
-        req.url = "http://example.com/"
-        assert link.selected(root, req)
-
-        req.url = "http://example.com"
-        assert link.selected(root, req)
+        req.view_name = ''
+        assert Link('').selected(Mock(__name__=None), req)
 
     def test_link_target(self):
         from kotti.util import Link
