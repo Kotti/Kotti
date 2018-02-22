7a3d5c5c86e70d1ac10d5abd7dcbdbfa6dc7f069
Andreas Kaiser <disko@binary-punks.com>
2018-02-22 15:22:24 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.0.0a1'
+version = '2.1.0a2.dev0'
 description = "A high-level, Pythonic web application framework based on " \
               "Pyramid and SQLAlchemy.  It includes an extensible Content " \
               "Management System called the Kotti CMS."
