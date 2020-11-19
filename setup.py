e89be16a086f179be9c9a861806a7d6270029870
Andreas Kaiser <andreas.kaiser@ipn.eu>
2020-11-19 18:13:56 +0100

    - Replace deprecated ``bleach_whitelist`` with its successor ``bleach-allowlist``. - Upgrade ``iso8601``.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ install_requires = [
     'Chameleon>=2.7.4',  # Fixes error when raising HTTPFound
     'alembic>=0.8.0',
     'bleach>=3.1.4',
-    'bleach-whitelist',
+    'bleach-allowlist',
     'colander>=1.3.2',
     'deform>=2.0.5',  # fixes file upload on py3 - uncomment after 2.0.5 is released  # noqa
     'docopt',
@@ -28,7 +28,7 @@ install_requires = [
     'filedepot',
     'formencode>=2.0.0a',
     'html2text',
-    'iso8601==0.1.11',  # rq.filter: !=0.1.12
+    'iso8601>=0.1.13',
     'js.angular',
     'js.bootstrap>=3.0.0',
     'js.deform>=2.0.3',
